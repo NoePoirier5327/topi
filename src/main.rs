ae31ad055ea403f6b333165bd70ca3225377309e
Noé Poirier <noer5327@gmail.com>
2026-08-18 19:53:24 +0200

    Ajout de stub pour les commandes.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,37 @@ mod stubs;
 mod topi;
 
 use topi::TopiEngine;
+use topi::commands::{CommandProcessor, CommandType};
 
 crate::impl_lua_stub! {
     class TopiEngine {
         fn run() -> "nil";
-        fn load_texture(path: "string", id: "number") -> "nil";
+        fn commands() -> "CommandProcessor";
+    }
+}
+
+crate::impl_lua_stub! {
+    class CommandProcessor {
+        fn new_anonyme_task(cmd_type: "CommandType", func: "fun(dt?: number)");
+        fn clear() -> "nil";
+    }
+}
+
+crate::impl_lua_enum_stub! {
+    enum CommandType {
+        Setup,
+        Update,
     }
 }
 
 fn main() {
-    let mut stub = TopiEngine::generate_stub();
+    let mut stub = String::new();
     stub.push_str("---@meta topi\n\n");
+
+    stub.push_str(&CommandType::generate_stub());
+    stub.push_str(&CommandProcessor::generate_stub());
+    stub.push_str(&TopiEngine::generate_stub());
+
     stub.push_str("---@class Topi\n");
     stub.push_str("local topi = {}\n\n");
     stub.push_str("--- Initialise une fenêtre de jeu.\n");
--- stubs/topi.lua
+++ stubs/topi.lua
@@ -1,15 +1,29 @@
+---@meta topi
+
+---@enum CommandType
+local CommandType = {
+    Setup = "Setup",
+    Update = "Update",
+}
+
+---@class CommandProcessor
+local CommandProcessor = {}
+
+---@param cmd_type CommandType
+---@param func fun(dt?: number)
+function CommandProcessor:new_anonyme_task(cmd_type, func) end
+
+---@return nil
+function CommandProcessor:clear() end
+
 ---@class TopiEngine
 local TopiEngine = {}
 
 ---@return nil
 function TopiEngine:run() end
 
----@param path string
----@param id number
----@return nil
-function TopiEngine:load_texture(path, id) end
-
----@meta topi
+---@return CommandProcessor
+function TopiEngine:commands() end
 
 ---@class Topi
 local topi = {}
