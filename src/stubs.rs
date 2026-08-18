ee27a79d8dc8d6d5d2eafe4e016b5a079f046a5c
Noé Poirier <noer5327@gmail.com>
2026-08-19 00:32:41 +0200

    Modification de la création de stub pour embarquer les champs des classes en plus des méthodes.

--- src/stubs.rs
+++ src/stubs.rs
@@ -1,22 +1,33 @@
 //! Module de génération de stubs pour le module lua.
 
 #[macro_export]
-macro_rules! impl_lua_stub {
+macro_rules! impl_lua_class_stub {
     (
         class $class_name:ident {
-            $(
-                fn $fn_name:ident($($arg:ident: $arg_ty:expr),*) $(-> $ret_ty:expr)?;
-            )*
+            $(fields {
+                $($field_name:ident: $field_ty:expr),* $(,)?
+            })?
+            $(methods {
+                $(
+                    fn $fn_name:ident($($arg:ident: $arg_ty:expr),*) $(-> $ret_ty:expr)?;
+                )*
+            })?
         }
     ) => {
         impl $class_name {
             pub fn generate_stub() -> String {
                 let mut stub = String::new();
                 stub.push_str(&format!("---@class {}\n", stringify!($class_name)));
+
+                // 1. Génération des champs (---@field)
+                $($(
+                    stub.push_str(&format!("---@field {} {}\n", stringify!($field_name), $field_ty));
+                )*)?
+
                 stub.push_str(&format!("local {} = {{}}\n\n", stringify!($class_name)));
 
-                $(
-                    // On type explicitement Vec<&str> pour éviter l'erreur E0282 quand il n'y a pas d'arguments
+                // 2. Génération des méthodes (functions)
+                $($(
                     let mut args_vec: Vec<&str> = Vec::new();
                     $(
                         stub.push_str(&format!("---@param {} {}\n", stringify!($arg), $arg_ty));
@@ -28,9 +39,8 @@ macro_rules! impl_lua_stub {
 
                     let args_str = args_vec.join(", ");
                     stub.push_str(&format!("function {}:{}({}) end\n\n", stringify!($class_name), stringify!($fn_name), args_str));
-                )*
+                )*)?
 
-                //stub.push_str(&format!("return {}\n", stringify!($class_name)));
                 stub
             }
         }
