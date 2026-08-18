6419b11ed213f2978199c0a68425f474281bb9e8
Noé Poirier <noer5327@gmail.com>
2026-08-19 01:28:20 +0200

    Nouvel exemple d'utilisation de topi, un rectangle qui rebondit sur les coins de l'écran.

---
+++ examples/bouncing_rectangle/main.lua
@@ -0,0 +1,27 @@
+local topi = require("topi")
+local app = topi.init("Bouncing rectangle", 1000, 1000)
+
+local SPEED = { x = math.random(400, 600), y = math.random(400, 600) }
+local RECT_SIZE = { w = math.random(50, 150), h = math.random(50, 150) }
+local pos = { x = math.random(0, 1000), y = math.random(0, 1000) }
+
+function update_rect(dt)
+  pos.x = pos.x + SPEED.x * dt
+  pos.y = pos.y + SPEED.y * dt
+
+  if pos.x <= 0 or pos.x + RECT_SIZE.w >= 1000 then
+    SPEED.x = -SPEED.x
+  end
+
+  if pos.y <= 0 or pos.y + RECT_SIZE.h >= 1000 then
+    SPEED.y = -SPEED.y
+  end
+end
+
+function draw_rect(renderer)
+  renderer:draw_colored_rect(pos.x, pos.y, RECT_SIZE.w, RECT_SIZE.h, { r = 255, g = 0, b = 0, a = 255 })
+end
+
+app:commands():new_anonym_task("Update", update_rect)
+app:commands():new_drawing_task(draw_rect)
+app:run()
