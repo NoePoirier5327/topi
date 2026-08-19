cf5c79d3508b0177a521e66daed3b7e7216d3e9b
Noé Poirier <noer5327@gmail.com>
2026-08-19 10:40:23 +0200

    Correction de problèmes de l'exemple.

--- examples/bouncing_rectangle/main.lua
+++ examples/bouncing_rectangle/main.lua
@@ -1,9 +1,10 @@
 local topi = require("topi")
 local app = topi.init("Bouncing rectangle", 1000, 1000)
 
-local SPEED = { x = math.random(400, 600), y = math.random(400, 600) }
+local directions = {-1, 1}
+local SPEED = { x = math.random(400, 600) * directions[math.random(1,2)], y = math.random(400, 600) *  directions[math.random(1,2)] }
 local RECT_SIZE = { w = math.random(50, 150), h = math.random(50, 150) }
-local pos = { x = math.random(0, 1000), y = math.random(0, 1000) }
+local pos = { x = math.random(RECT_SIZE.w, 1000 - RECT_SIZE.w), y = math.random(RECT_SIZE.h, 1000 - RECT_SIZE.h) }
 
 function update_rect(dt)
   pos.x = pos.x + SPEED.x * dt
